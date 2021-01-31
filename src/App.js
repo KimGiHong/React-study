5c5711a6cc4d3f1f03bba3cd279fdf928d4f2e13
김기홍 <kimgihong9@naver.com>
2021-01-31 23:10:33 +0900

    React-Study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Helllo from './Hello';
 import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
-  const name = 'react'
-  const style = {
-    backgroundColor : 'black',
-    color : 'aqua',
-    padding : '1rem'
-  };
   return (
-    <>
-    {/* jsx 내부에서 주석을 사용하려면 {}를 사용하자.*/}
-      <Helllo 
-        //이런식으로 작성하는 주석은 화면에 나타나지 않는다.
-      /> 
-      <div style = {style}>{name}</div>
-      <div className = "gray-box"></div>
-    </>
+    <Wrapper>
+      <Helllo name = "react" color = "red" isSpecial={true}/>
+      <Helllo color = "pink"/>
+    </Wrapper>
+    // <>
+    // {/* jsx 내부에서 주석을 사용하려면 {}를 사용하자.*/}
+    //   <Helllo 
+    //     //이런식으로 작성하는 주석은 화면에 나타나지 않는다.
+    //   /> 
+    //   <div style = {style}>{name}</div>
+    //   <div className = "gray-box"></div>
+    // </>
   );
 }
+export default App;
 
 /*
 
@@ -29,5 +29,18 @@ jsx 내부에서 javascript의 값을 나타내고싶다면 {}를 사용한다.
 jsx에서 인라인 스타일을 설정할 때에는 객체를 생성하고 위와 같은 방법으로 작성하면 된다.
 jsx에서는 class 대신 className이란걸 사용한다.
 
+
+*Props
+
+name과 color 같은 것들을 props라고 칭한다.
+Hello컴포넌트에서 name,color라는 props값을 파라미터를 통해 받아올 수 있는데 비구조화 할당을 해주면 바로 추출하여 사용할 수 있다.
+기본값을 사용하기 위해서 defaultprops를 이용하도록 하자.
+태그와 태그 사이에 넣는 내용을 의미하는것이 children props 이다.
+
+
+*조건부 렌더링
+:특정 조건이 참인지 거짓인지에 따라서 다른 결과를 보여주는 것을 의미한다.
+조건부 렌더링을 사용할때에는 삼항연산자 또는 and연산자를 사용하자.
+isSpecial의 값에 아무런 값을 넣지 않으면 true값을 출력함.
+
 */
-export default App;
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-function Hello() {
-    return <div>안녕하세요</div>
+function Hello({color , name , isSpecial}) {
+    return ( 
+        <div style ={{
+            color
+        }}>
+            <b>{isSpecial ? '특별한' : '특별하지 않은'}</b>
+            안녕하세요{name}
+        </div>
+    );
 }
 
+Hello.defaultProps = {
+    name: '이름없음'
+}
 export default Hello;
